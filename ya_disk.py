0f321a2450cf47ae9a089ad1db4e326d7fedbbc6
mrGeist469 <75024897+mrGeist469@users.noreply.github.com>
2021-04-28 16:00:34 +0300

    reformat code

--- ya_disk.py
+++ ya_disk.py
@@ -1,13 +1,24 @@
+import requests
+
+
 class YaUploader:
-    def __init__(self, file_path: str):
+    def __init__(self, token, file_path: str):
         self.file_path = file_path
+        self.token = token
+        print(file_path[41:])
 
-    def upload(self):
+    def upload(self, disk_file_path):
         """Метод загруджает файлы по списку file_list на яндекс диск"""
-        # Тут ваша логика
-        return 'Вернуть ответ об успешной загрузке'
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token)
+        }
+        params = {"path": disk_file_path, "overwrite": "true"}
+        response = requests.get(upload_url, headers=headers, params=params)
+        file_json = response.json()
+        print(file_json)
 
-
-if __name__ == '__main__':
-    uploader = YaUploader('c:\my_folder\file.txt')
-    result = uploader.upload()
+        # response.raise_for_status()
+        # if response.status_code == 201:
+        #     print("Success")
