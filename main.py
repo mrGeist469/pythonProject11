3dd67fdeed0b898eeb336a140d63d7e7e275ea77
mrGeist469 <75024897+mrGeist469@users.noreply.github.com>
2021-04-28 17:09:29 +0300

    finish

---
+++ main.py
@@ -0,0 +1,6 @@
+from ya_disk import YaUploader
+
+if __name__ == '__main__':
+    ya = YaUploader('',
+                    'C:\\Users\Public\Pictures\Sample Pictures\Penguins.jpg')
+    ya.upload()
--- ya_disk.py
+++ ya_disk.py
@@ -5,20 +5,19 @@ class YaUploader:
     def __init__(self, token, file_path: str):
         self.file_path = file_path
         self.token = token
-        print(file_path[41:])
 
-    def upload(self, disk_file_path):
+    def upload(self):
         """Метод загруджает файлы по списку file_list на яндекс диск"""
         upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
         headers = {
             'Content-Type': 'application/json',
             'Authorization': 'OAuth {}'.format(self.token)
         }
-        params = {"path": disk_file_path, "overwrite": "true"}
+        params = {"path": self.file_path[41:], "overwrite": "true"}
         response = requests.get(upload_url, headers=headers, params=params)
         file_json = response.json()
         href = file_json.get("href", "")
         response_load = requests.put(href, data=open(self.file_path, 'rb'))
         response_load.raise_for_status()
-        if response.status_code == 201:
+        if response_load.status_code == 201:
             print("Success")
